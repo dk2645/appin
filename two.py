8e566a50f1a50b7961bf78e9c20e8ce8514e54fc
root <root@ip-172-31-41-122.ap-south-1.compute.internal>
2023-04-19 05:08:03 +0000

    date calender

---
+++ two.py
@@ -0,0 +1,15 @@
+year = int(input("year ") )
+m = int(input("month range (1-12): ") )
+ if m > 12:
+     print("retry")
+ else:
+     if m == 2:
+         if year % 4 == 0 and (year % 100 != 0 or year % 400 == 0):
+             d = 29
+         else:
+             d = 28
+     elif m in [4, 6, 9, 11]:
+         d = 30
+     else:
+         d = 31
+     print("No. of days :", d)
